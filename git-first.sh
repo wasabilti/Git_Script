a955df5f0342efb51871c1a73181f1d04102d85c
wasabilti <2923043785@qq.com>
2020-05-09 16:38:48 +0800

    add script for first install

---
+++ git-first.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Use to init git at first install
+
+PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Library/TeX/texbin:/Library/Apple/usr/bin:/Users/wasabi/bin:/Users/wasabi/bin:/Users/wasabi/bin
+
+export PATH
+
+read -p "Username: " USERNAME
+read -p "Email: " EMAIL
+
+git config --global user.name "$USERNAME"
+git config --global user.email "$EMAIL"
+
+
+ssh-keygen -t rsa -C "EMAIL"
+
+eval $(ssh-agent -s)
+
+read -p "Private key file name(Incoude path): " KEYNAME
+
+ssh-add $KEYNAME
+
+echo 'Copy the pub key to github.com'
+
+read tmp_wait
+
+ssh -T git@github.com
